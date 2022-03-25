a6dfd1790d10b0cf9bdcd67537a06df6f131c47c
joannayhs <joannayuhas@gmail.com>
2022-03-25 12:15:45 -0400

    completed changes to pass tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, {useState, useEffect} from 'react'
+
+function App(){
+    const [isLoading, setIsLoading] = useState(true)
+    const [imgUrl, setImgUrl] = useState('')
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then( result => {
+            setImgUrl(result.message)
+            setIsLoading(false)
+        })
+    })
+
+    return (
+        <div>
+            {isLoading ? <p>Loading...</p> : <img src={imgUrl} alt="A Random Dog"/>}
+        </div>
+    )
+}
+
+export default App
+
